c223c0f9dc6a7bb6bb4de1c77e9fa93aebdd9b9c
nevtay <nevintay@gmail.com>
2020-01-08 23:06:05 +0800

    added minus, multiply, and divide functions

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const add = function(a, b) {
+    console.log(`${a} is being added to ${b}`);
     return a + b;
 }
 
+const minus = function(a, b) {
+    console.log(`${a} is being minused by ${b}`);
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    console.log(`${a} is being multiplied by ${b}`);
+    return a * b;
+}
+
+const divide = function(a, b) {
+    console.log(`${a} is being divided by ${b}`);
+    return a / b;
+}
+
 const math = {
-    add: add
+    add: add,
+    minus: minus,
+    multiply: multiply,
+    divide: divide
 }
 
 module.exports = math;
