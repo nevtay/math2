d172a0b007efa427feacd8899a7b50e741677100
nevtay <nevintay@gmail.com>
2020-01-10 16:51:39 +0800

    updated Add and Multiply functions to accept Rest parameters; added new math method (powOf)

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const add = function(a, b) {
-    console.log(`${a} is being added to ${b}`);
-    return a + b;
-}
-
-const minus = function(a, b) {
+const add = function(...numbers) {
+    return numbers.reduce((acc, num) => acc + num);
+  };
+  
+  const minus = function(a, b) {
     console.log(`${a} is being minused by ${b}`);
     return a - b;
-}
-
-const multiply = function(a, b) {
+  };
+  
+  const multiply = function(...numbers) {
     console.log(`${a} is being multiplied by ${b}`);
     return a * b;
-}
-
-const divide = function(a, b) {
+  };
+  
+  const divide = function(a, b) {
     console.log(`${a} is being divided by ${b}`);
     return a / b;
-}
-
-const math = {
+  };
+  
+  const powOf = function(a, b) {
+    console.log(`${a} to the power of ${b}`);
+    return a ** b;
+  };
+  
+  const math = {
     add: add,
     minus: minus,
     multiply: multiply,
-    divide: divide
-}
-
-module.exports = math;
+    divide: divide,
+    powOf: powOf
+  };
+  
+  module.exports = math;
+  
